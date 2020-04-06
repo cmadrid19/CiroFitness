402ccc2610f55d265bd2be1fa0e326f65f88e38b
cmadrid19 <maxim.macari.dam@gmail.com>
2020-04-06 21:29:08 +0200

    alerter

--- CiroFitness/app/src/main/java/com/example/cirofitness/Activities/MainActivity.kt
+++ CiroFitness/app/src/main/java/com/example/cirofitness/Activities/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.cirofitness.Activities
 
+import android.content.Intent
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.example.cirofitness.R
+import com.tapadoo.alerter.Alerter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +22,21 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+
+    /**
+     * Alerta: aviso 'Sino conexion a internet'
+     */
+    fun NetworkAlert(){
+        Alerter.create(this)
+            .setTitle("No conectado")
+            .setText("No hay conexion a intenet")
+            .setDuration(3500)
+            .setIcon(R.drawable.network_off)
+            .enableSwipeToDismiss()
+            .setOnClickListener(
+                View.OnClickListener { startActivityForResult(Intent(Settings.ACTION_WIFI_SETTINGS), 0) })
+            .show()
     }
 }
